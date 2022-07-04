564276191e52f541d51b94aed1334edca075ad12
Abbas-Sayyed99 <106953462+Abbas-Sayyed99@users.noreply.github.com>
2022-07-04 16:15:26 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,16 @@
+class Pattern1
+{
+	public static void main(String[] args)
+	{
+		int i,j;
+
+		for(i=65; i<=69; i++)
+		{
+			for(j=65; j<=i; j++)
+			{
+				System.out.print((char)j+" ");
+			}
+			System.out.println();
+		}
+	}	
+}
